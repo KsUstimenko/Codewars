518ccac9404bd10e747a3c37a43f3ed23b19d219
kseniia <ksusha.mamatova98@gmail.com>
2022-07-27 14:22:22 +0300

    finished test16

---
+++ test17.js
@@ -0,0 +1,14 @@
+// Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+
+
+function multipleOfIndex(array) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % i === 0) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+}
+
+console.log(multipleOfIndex([22, -6, 32, 82, 9, 25]));
